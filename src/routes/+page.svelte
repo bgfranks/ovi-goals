9b93c663642d3bbd7f890c6ad4e0fab6658ceaeb
Brandon Franks <bgfranks.dev@gmail.com>
2023-01-06 22:28:55 -0500

    created the about page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <div
-	class="text-center w-[80%] md:w-[60%] h-[60%] px-10 py-10 bg-black bg-opacity-50 rounded-lg backdrop-blur-sm flex flex-col gap-4 justify-center"
+	class="text-center w-[80%] md:w-[60%] h-[60%] px-10 py-10 bg-black bg-opacity-60 rounded-lg backdrop-blur-sm flex flex-col gap-4 justify-center"
 >
 	<h3 class="text-3xl md:text-4xl text-gray-200 font-bold">Ovi Goals</h3>
 	<span class="logo text-8xl md:text-[10rem] font-bold">{totalGoals}</span>
