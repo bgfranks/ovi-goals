d23edd51de4b98381e7c356843a5a6038c0bba84
Brandon Franks <bgfranks.dev@gmail.com>
2023-01-06 21:09:21 -0500

    added the background and created the header

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,17 @@
+<div
+	class="fixed top-0 flex px-10 bg-gray-900 bg-opacity-60 w-full md:w-[80%] lg:w-[65%] text-white justify-between items-center py-3 md:rounded-r-full"
+>
+	<a href="/">
+		<img src="caps.png" alt="Washington Capitals Logo" class="w-[45px]" />
+	</a>
+	<nav class="md:pr-10">
+		<ul class="flex gap-10 md:gap-16 text-lg">
+			<li class="hover:text-gray-300 hover:scale-110 transition-all duration-400">
+				<a href="/">Home</a>
+			</li>
+			<li class="hover:text-gray-300 hover:scale-110 transition-all duration-400">
+				<a href="/about">About</a>
+			</li>
+		</ul>
+	</nav>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script>
+	import Header from '$lib/components/Header.svelte';
 	import '../app.css';
 </script>
 
-<main class="bg-gray-900 h-screen text-white">
+<Header />
+<main
+	class="bg-gray-900 h-screen text-white bg-ovi bg-cover bg-no-repeat bg-center overflow-hidden"
+>
 	<slot />
 </main>
