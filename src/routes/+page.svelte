3de45374e9e04c0123fc70ddca47b54d860213e9
Brandon Franks <bgfranks.dev@gmail.com>
2023-01-06 22:07:37 -0500

    playing around with the styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,8 +8,10 @@
 	$: ({ totalGoals } = data);
 </script>
 
-<div class="text-center absolute bg-black bg-opacity-50 px-40 py-28 rounded-lg backdrop-blur-sm">
-	<h3 class="text-2xl text-gray-200 pb-5 font-bold">Ovi Goals</h3>
-	<span class="text-6xl font-bold">{totalGoals}</span>
-	<p class="text-lg pt-5 text-gray-200">{894 - totalGoals} goals until #1</p>
+<div
+	class="text-center w-[80%] md:w-[60%] h-[60%] px-10 py-10 bg-black bg-opacity-50 rounded-lg backdrop-blur-sm flex flex-col gap-4 justify-center"
+>
+	<h3 class="text-3xl md:text-4xl text-gray-200 font-bold">Ovi Goals</h3>
+	<span class="logo text-8xl md:text-[10rem] font-bold">{totalGoals}</span>
+	<p class="text-xl text-gray-200">{894 - totalGoals} goals until #1</p>
 </div>
